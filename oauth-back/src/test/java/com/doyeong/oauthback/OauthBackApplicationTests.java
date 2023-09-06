3cc007d569ff6135f0747997957f61dcb1df91a1
DoYeongK <kodo7736@naver.com>
2023-09-06 15:26:39 +0900

    2023-09-06 15:20

---
+++ oauth-back/src/test/java/com/doyeong/oauthback/OauthBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doyeong.oauthback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
