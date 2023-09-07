d4eb8a7ec70a6340ebbf4b5b0b636638559050eb
DoYeongK <kodo7736@naver.com>
2023-09-07 17:56:57 +0900

    2023-09-07 로그인 오류 시 작업하는 방법

--- oauth-front/src/views/Oauth/index.tsx
+++ oauth-front/src/views/Oauth/index.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useCookies } from 'react-cookie';
 
 export default function Oauth() {
+
+  const { token } = useParams();
+  const [cookies, setCookie] = useCookies();
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    if (!token) return;
+    setCookie('token', token, { path: '/' });
+    navigator('/');
+  },[token]);
+
   return (
-    <div>Oauth</div>
+    <div>Oauth : {token}</div>
   )
 }
